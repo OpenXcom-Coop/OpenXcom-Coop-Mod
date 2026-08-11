5af6e5803c8a2945a6946d7141c7f73d0ccaead0
xcomcoopdev <xcom.coop.dev@gmail.com>
2026-08-11 10:21:40 +0300

    Update src/Savegame/SavedGame.cpp
    Co-authored-by: NonPolynomialTim <10065854+NonPolynomialTim@users.noreply.github.com>

--- src/Savegame/SavedGame.cpp
+++ src/Savegame/SavedGame.cpp
@@ -2474,7 +2474,13 @@ int SavedGame::getBaseMaintenance() const
 		if (connectionTCP::isSharedCampaignStatic() == false && connectionTCP::getCoopStatic() == true && xbase->_coopBase == false)
 		{
 			total += xbase->getMonthlyMaintenace();
+		// coop
+		// In separate campaigns, monthly maintenance costs only include bases owned by the current player, not bases owned by other players. This is a temporary fix until the separate campaign rework.
+		if (connectionTCP::getCoopStatic() == true && connectionTCP::isSharedCampaignStatic() == false && xbase->_coopBase == true)
+		{
+			continue;
 		}
+		total += xbase->getMonthlyMaintenace();
 	}
 	return total;
 }
